149bd67ba9dcfa16545428127d421e440e7cbea1
Fruit Lee <glfruit80@gmail.com>
2019-11-24 18:28:56 +0800

    Add fontawesome library

---
+++ src/components/HomeList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4>
+                <a href="/location/590d8dc7a7cb5b8e3f1bfc48">Costy</a>
+                <small>&nbsp;
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                </small>
+                <span class="badge badge-pill badge-default float-right">14.0km</span>
+            </h4>
+            <p class="address">High Street, Reading</p>
+            <div class="facilities">
+                <span class="badge badge-warning">hot drinks</span> <span class="badge badge-warning">food</span>
+                <span class="badge badge-warning">power</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeList'
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.use(BootstrapVue)
 
